ee2536a13f066e8c533441627bcd3e19996ec15f
Siam <158517313+Niamor0@users.noreply.github.com>
2024-12-14 12:27:43 +0600

    Update views.py

--- foodorder/views.py
+++ foodorder/views.py
@@ -1,1 +1,17 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.contrib import messages
+from .models import *
+from django.db.models import Count, Sum, F, Q
+from decimal import Decimal
+from django.utils import timezone
+from django.http import JsonResponse, Http404
 
+
+def Home(request):
+    return render(request, 'home.html')
+
+def is_admin(user):
+    return user.is_staff
